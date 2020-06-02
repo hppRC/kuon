a943442a74947c3256bebf58f25496a6b208ccc4
hppRC <hpp.ricecake@gmail.com>
2020-06-02 23:13:09 +0900

    :art: Change format

--- src/tweets/favorite.rs
+++ src/tweets/favorite.rs
@@ -4,9 +4,7 @@ use anyhow::Result;
 impl TwitterAPI {
     pub async fn favorite(&self, id: &str) -> Result<FavoriteResult> {
         let endpoint = "https://api.twitter.com/1.1/favorites/create.json";
-        let params = maplit::hashmap! {
-            "id" => id,
-        };
+        let params = maplit::hashmap! { "id" => id };
 
         let favorite_results: FavoriteResult = self.post(endpoint, &params).await?;
         Ok(favorite_results)
