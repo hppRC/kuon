2dde4040d4f10b1f7244dc2873dc1028ea8db893
hppRC <hpp.ricecake@gmail.com>
2020-06-03 00:36:50 +0900

    :+1: Make examples easier

--- examples/search.rs
+++ examples/search.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
     // Please set API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET in environment
-    let api: kuon::TwitterAPI = kuon::TwitterAPI::new_from_env().await?;
+    let api: kuon::TwitterAPI = kuon::TwitterAPI::new_using_env().await?;
     let res: kuon::SearchResult = api.search_tweets("rust").await?;
 
     for tweet in res.statuses {
