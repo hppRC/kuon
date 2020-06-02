61331c1960662dfe3c0bcb4afe799e720eb921e9
hppRC <hpp.ricecake@gmail.com>
2020-06-02 23:12:47 +0900

    :sparkles: Add some basic types

--- src/models/result.rs
+++ src/models/result.rs
@@ -1,10 +1,11 @@
+use crate::models::*;
 use serde_derive::*;
 use serde_json::Value;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct SearchResult {
-    pub search_metadata: Value,
-    pub statuses: Vec<Value>,
+    pub search_metadata: SearchMetadata,
+    pub statuses: Vec<Tweet>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
@@ -22,4 +23,18 @@ pub struct RetweetResult {
 pub struct TweetResult {
     pub created_at: Value,
     pub text: Value,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct SearchMetadata {
+    pub completed_in: f64,
+    pub count: u64,
+    // I'm afraid of overflows...
+    pub max_id: u64,
+    pub max_id_str: String,
+    pub next_results: Option<String>,
+    pub query: String,
+    pub refresh_url: Option<String>,
+    pub since_id: u64,
+    pub since_id_str: String,
 }
--- src/models/twitter_api.rs
+++ src/models/twitter_api.rs
@@ -13,6 +13,7 @@ pub struct TwitterAPI {
 }
 
 impl TwitterAPI {
+<<<<<<< HEAD
     /// Creates [`ClientBuilder`] that helps construct a configured `TwitterAPI`.
     ///
     /// This is exactly equivalent to [`ClientBuilder::new`].
@@ -21,6 +22,62 @@ impl TwitterAPI {
     /// [`ClientBuilder::new`]: struct.ClientBuilder.html#method.new
     pub fn builder() -> ClientBuilder<(), (), (), ()> {
         ClientBuilder::new()
+=======
+    pub async fn new(
+        api_key: &str,
+        api_secret_key: &str,
+        access_token: &str,
+        access_token_secret: &str,
+    ) -> Result<Self> {
+        let client = reqwest::Client::new();
+        let bearer: BearerToken = Self::get_bearer(api_key, api_secret_key, &client).await?;
+        Ok(Self {
+            access_token: access_token.into(),
+            access_token_secret: access_token_secret.into(),
+            api_key: api_key.into(),
+            api_secret_key: api_secret_key.into(),
+            bearer,
+            client,
+        })
+    }
+
+    pub async fn new_from_env() -> Result<Self> {
+        let access_token = &std::env::var("ACCESS_TOKEN")?;
+        let access_token_secret = &std::env::var("ACCESS_TOKEN_SECRET")?;
+        let api_key = &std::env::var("API_KEY")?;
+        let api_secret_key = &std::env::var("API_SECRET_KEY")?;
+        Self::new(api_key, api_secret_key, access_token, access_token_secret).await
+    }
+
+    async fn get_bearer(
+        api_key: &str,
+        api_secret_key: &str,
+        client: &reqwest::Client,
+    ) -> Result<BearerToken> {
+        let endpoint = "https://api.twitter.com/oauth2/token";
+        let encoded_keys = base64::encode(&format!("{}:{}", api_key, api_secret_key));
+        let header_auth = format!("Basic {}", encoded_keys);
+
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, header_auth.parse()?);
+        headers.insert(
+            CONTENT_TYPE,
+            HeaderValue::from_static("application/x-www-form-urlencoded"),
+        );
+
+        // TODO: #8 better error handling
+        let text = client
+            .post(endpoint)
+            .body("grant_type=client_credentials")
+            .headers(headers)
+            .send()
+            .await?
+            .text()
+            .await?;
+
+        let bearer: BearerToken = serde_json::from_str(&text)?;
+        Ok(bearer)
+>>>>>>> b91ec64... :sparkles: Add some basic types
     }
 }
 
