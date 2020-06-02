b91ec64e93710c673e39421b1bef7aa441de4067
hppRC <hpp.ricecake@gmail.com>
2020-06-02 23:12:47 +0900

    :sparkles: Add some basic types

--- src/models/tweet.rs
+++ src/models/tweet.rs
@@ -1,36 +1,38 @@
-// #[derive(Debug, Clone, Deserialize)]
-// pub(crate) struct RawTweet {
-//     pub coordinates: Option<RawCoordinates>,
-//     #[serde(deserialize_with = "deserialize_datetime")]
-//     pub created_at: chrono::DateTime<chrono::Utc>,
-//     pub current_user_retweet: Option<CurrentUserRetweet>,
-//     pub display_text_range: Option<(usize, usize)>,
-//     pub entities: TweetEntities,
-//     pub extended_entities: Option<ExtendedTweetEntities>,
-//     pub extended_tweet: Option<RawExtendedTweet>,
-//     pub favorite_count: i32,
-//     pub favorited: Option<bool>,
-//     pub filter_level: Option<FilterLevel>,
-//     pub id: u64,
-//     pub in_reply_to_user_id: Option<u64>,
-//     pub in_reply_to_screen_name: Option<String>,
-//     pub in_reply_to_status_id: Option<u64>,
-//     pub lang: Option<String>,
-//     pub place: Option<place::Place>,
-//     pub possibly_sensitive: Option<bool>,
-//     pub quoted_status_id: Option<u64>,
-//     pub quoted_status: Option<Box<Tweet>>,
-//     pub retweet_count: i32,
-//     pub retweeted: Option<bool>,
-//     pub retweeted_status: Option<Box<Tweet>>,
-//     #[serde(deserialize_with = "deserialize_tweet_source")]
-//     pub source: Option<TweetSource>,
-//     pub text: Option<String>,
-//     pub full_text: Option<String>,
-//     pub truncated: bool,
-//     pub user: Option<Box<user::TwitterUser>>,
-//     #[serde(default)]
-//     pub withheld_copyright: bool,
-//     pub withheld_in_countries: Option<Vec<String>>,
-//     pub withheld_scope: Option<String>,
-// }
+use serde_derive::*;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Tweet {
+    // pub coordinates: Option<RawCoordinates>,
+    // #[serde(deserialize_with = "deserialize_datetime")]
+    pub created_at: String,
+    // pub current_user_retweet: Option<CurrentUserRetweet>,
+    pub display_text_range: Option<(usize, usize)>,
+    // pub entities: TweetEntities,
+    // pub extended_entities: Option<ExtendedTweetEntities>,
+    // pub extended_tweet: Option<RawExtendedTweet>,
+    pub favorite_count: u64,
+    pub favorited: Option<bool>,
+    // pub filter_level: Option<FilterLevel>,
+    pub id: u64,
+    pub in_reply_to_user_id: Option<u64>,
+    pub in_reply_to_screen_name: Option<String>,
+    pub in_reply_to_status_id: Option<u64>,
+    pub lang: Option<String>,
+    // pub place: Option<place::Place>,
+    pub possibly_sensitive: Option<bool>,
+    pub quoted_status_id: Option<u64>,
+    pub quoted_status: Option<Box<Tweet>>,
+    pub retweet_count: u64,
+    pub retweeted: Option<bool>,
+    pub retweeted_status: Option<Box<Tweet>>,
+    // #[serde(deserialize_with = "deserialize_tweet_source")]
+    // pub source: Option<TweetSource>,
+    pub text: String,
+    // pub full_text: Option<String>,
+    pub truncated: bool,
+    // pub user: Option<Box<user::TwitterUser>>,
+    #[serde(default)]
+    pub withheld_copyright: bool,
+    pub withheld_in_countries: Option<Vec<String>>,
+    pub withheld_scope: Option<String>,
+}
--- src/models/twitter_api.rs
+++ src/models/twitter_api.rs
@@ -32,6 +32,14 @@ impl TwitterAPI {
         })
     }
 
+    pub async fn new_from_env() -> Result<Self> {
+        let access_token = &std::env::var("ACCESS_TOKEN")?;
+        let access_token_secret = &std::env::var("ACCESS_TOKEN_SECRET")?;
+        let api_key = &std::env::var("API_KEY")?;
+        let api_secret_key = &std::env::var("API_SECRET_KEY")?;
+        Self::new(api_key, api_secret_key, access_token, access_token_secret).await
+    }
+
     async fn get_bearer(
         api_key: &str,
         api_secret_key: &str,
