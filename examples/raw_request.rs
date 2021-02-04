a573ecb389f71635d8dc687fc9d0c1832da1027b
uzimaru0000 <shuji365630@gmail.com>
2021-02-04 14:34:57 +0900

    add example
    fix request methods

---
+++ examples/raw_request.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+use anyhow::Result;
+use maplit::hashmap;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Please set API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET in environment
+    let api: kuon::TwitterAPI = kuon::TwitterAPI::new_using_env().await?;
+    let url = "https://api.twitter.com/1.1/account/update_profile.json";
+    let res: HashMap<String, String> = api
+        .raw_post(url, &hashmap! { "name" => "new_user_name" })
+        .await?;
+
+    println!("{:?}", res);
+    Ok(())
+}
