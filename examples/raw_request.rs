fbcf6bd1182f1dbbd97542345b5f2d785bce62de
hppRC <hpp.ricecake@gmail.com>
2021-03-06 19:19:29 +0900

    :+1: Update examples and tests

--- examples/raw_request.rs
+++ examples/raw_request.rs
@@ -7,10 +7,10 @@ async fn main() -> Result<()> {
     // Please set API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET in environment
     let api: kuon::TwitterAPI = kuon::TwitterAPI::new_using_env().await?;
     let url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-    let res: Vec<kuon::Tweet> = api.raw_get(url, &HashMap::new(), None).await?;
+    let res: Vec<kuon::Tweet> = api.raw_get(url, &HashMap::new()).await?;
 
     for tweet in res {
-        println!("{:?}", tweet);
+        println!("{}", tweet.id);
     }
 
     Ok(())
